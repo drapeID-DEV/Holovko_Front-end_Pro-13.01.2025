4797d8e32cb25e48a1c0bba6f8d06bf6abe20388
drapeID-DEV <drapeDEV@gmail.com>
2025-02-06 20:43:18 +0200

    hw9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let result = "";
 
-for (let i = 10; i <= 20; i++) {
-    if (i < 20) {
+for (let i = 20; i <= 30; i += 0.5) {
+    if (i < 30) {
         result += i + `, `;
     } else {
         result += i;
@@ -12,120 +12,47 @@ alert(result)
 
 /////////////
 
+let rate = 40;
+
+for (let i = 10; i <= 100; i += 10) {
+    alert(`${i} * 40 = ${i * rate}`);
+}
+
+//////////////
+
+let num;
+
 result = "";
 
-for (let i = 10; i <= 20; i++) {
-    result += i * i + ","
+while (num === "" || isNaN(num) || num == undefined) {
+    num = prompt("Please input the number: ")
+}
+
+for (let i = 1; i <= 100; i++) {
+    if (i ** 2 < num) {
+        result += i + `,`
+    }
 }
 
 alert(result)
 
-/////////////
+///////////////
 
-for (let i = 1; i <= 10; i++) {
-    alert(`7 * ${i} = ${i * 7}`);
-}
-
-/////////////
-
-result = 0;
-
-for (let i = 1; i <= 15; i++) {
-    result += i;
-}
-
-alert(result);
-
-/////////////
-
-result = 0;
-
-for (let i = 15; i <= 35; i++) {
-    result *= i;
-}
-
-alert(result);
-
-////////////
-
-result = 0;
-
-for (let i = 1; i <= 500; i++) {
-    result += i;
-}
-
-alert(result);
-
-/////////////
-
-result = 0;
-
-for (let i = 30; i <= 80; i++) {
-    if (i % 2 == 0) {
-        result += i;
-    }
-}
-
-alert(result);
-
-/////////////
-
-result = "";
-
-for (let i = 100; i <= 200; i++) {
-    if (i % 3 == 0) {
-        result += i + `,`;
-    }
-}
-
-alert(result);
-
-//////////////
-
-result = "";
-
-let num;
-
-while (isNaN(num) || num === "" || num == undefined) {
-    num = prompt("Please input the number: ");
-}
-
-for (let i = 0; i <= num; i++) {
+for (let i = 2; i <= num - 1; i++) {
     if (num % i == 0) {
-        result += i + `,`;
+        alert("This is prime number");
+        break;
     }
 }
-
-alert(result);
-
-////////////////
-
-let counter = 0;
-
-for (let i = 0; i <= num; i++) {
-    if (num % i == 0 && i % 2 == 0) {
-        counter++;
-    }
-}
-
-alert(counter);
 
 //////////////
 
-result = 0;
-
-for (let i = 0; i <= num; i++) {
-    if (num % i == 0 && i % 2 == 0) {
-        result += i;
-    }
-}
-
-alert(result);
-
-//////////////
-
-for (let i = 1; i <= 10; i++) {
-    for (let j = 1; j <= 10; j++) {
-        alert(`${i} * ${j} = ${i * j}`);
+for (let i = 0; i < num; i++) {
+    if (num == 3 ** i) {
+        alert("We can get a number this way")
+        break;
+    } else if (3 ** i > num) {
+        alert("We can't get this number")
+        break;
     }
 }
