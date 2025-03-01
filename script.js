9b6d45e9062a61193c64c4346d82d364ade928a2
drapeID-DEV <drapeDEV@gmail.com>
2025-03-01 21:36:50 +0200

    done hw

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
-// let result = 0;
+let result = 0;
 
-// function sumPrev(num) {
-//   result += num;
-//   return result;
-// }
+function sumPrev(num) {
+  result += num;
+  return result;
+}
 
-// console.log(result)
-// console.log(sumPrev(2))
-// console.log(sumPrev(3))
+console.log(result)
+console.log(sumPrev(2))
+console.log(sumPrev(3))
 
-// ////////
+////////
 
-// let testArr = ["123", 24, "name", 12, 0, 2, "qwerty"]
+let testArr = ["123", 24, "name", 12, 0, 2, "qwerty"]
 
-// function arithmeticMean(arr) {
-//   let sum = 0;
+function arithmeticMean(arr) {
+  let sum = 0;
 
-//   for(let i = 0; i < arr.length; i++) {
-//     if(typeof arr[i] === 'number') {
-//       sum += arr[i]
-//     }
-//   }
+  for(let i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === 'number') {
+      sum += arr[i]
+    }
+  }
 
-//   return sum / arr.length;
-// }
+  return sum / arr.length;
+}
 
-// console.log(arithmeticMean(testArr));
+console.log(arithmeticMean(testArr));
 
-// ////////////
+////////////
 
-// function doMath(x, znak, y) {
-//   switch(znak){
-//     case '+':
-//       return x + y;
-//     case '-':
-//       return x - y;
-//     case '*':
-//       return x * y;
-//     case '/':
-//       return x / y;
-//     case '%':
-//       return x % y;
-//     default:
-//       console.log("Invalid data");
-//       return false;
-//   }
-// }
+function doMath(x, znak, y) {
+  switch(znak){
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '*':
+      return x * y;
+    case '/':
+      return x / y;
+    case '%':
+      return x % y;
+    default:
+      console.log("Invalid data");
+      return false;
+  }
+}
 
-// console.log(doMath(4, "*", 5));
+console.log(doMath(4, "*", 5));
 
-///////////
+/////////
 
-// function setArray() {
-//   let arr = [];
+function setArray() {
+  let arr = [];
 
-//   let exLength;
-//   do {
-//     exLength = prompt("Please input the length of external array:: ");
-//   } while (exLength === null || exLength.trim() === "" || isNaN(exLength))
+  let exLength;
+  do {
+    exLength = prompt("Please input the length of external array:: ");
+  } while (exLength === null || exLength.trim() === "" || isNaN(exLength))
 
-//   let inLength;
-//   do {
-//     inLength = prompt("Please input the length of internal array: ");
-//   } while (inLength === null || inLength.trim() === "" || isNaN(inLength))
+  let inLength;
+  do {
+    inLength = prompt("Please input the length of internal array: ");
+  } while (inLength === null || inLength.trim() === "" || isNaN(inLength))
 
-//   for(let i = 0; i < +exLength; i++) {
-//     arr[i] = [];
-//     for(let j = 0; j < +inLength; j++) {
-//       arr[i][j] = prompt(`Please input [${i}][${j}] element of array: `)
-//     }
-//   }
+  for(let i = 0; i < +exLength; i++) {
+    arr[i] = [];
+    for(let j = 0; j < +inLength; j++) {
+      arr[i][j] = prompt(`Please input [${i}][${j}] element of array: `)
+    }
+  }
 
-//   return arr;
-// }
+  return arr;
+}
 
-// let array = setArray();
+let array = setArray();
 
-// console.log(array);
+console.log(array);
 
 /////////////
 
